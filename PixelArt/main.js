8d708d6d120e104f25e97f19d2ca0f3287cf52c0
Taylor Brown <46949572+Deathwalker568@users.noreply.github.com>
2019-05-29 13:53:07 -0600

    5/29

---
+++ PixelArt/main.js
@@ -0,0 +1,17 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+var penColor = 'black';
+function setPenColor(pen)
+{
+    penColor = pen;
+}
+function setPixelColour(pixel)
+{
+    pixel.style.backgroundColor = penColor;
+}
